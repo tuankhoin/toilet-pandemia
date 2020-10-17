2026f601996845abb4c44fc1c6d7b5d68e78264a
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-10-17 16:34:03 +1100

    Update EnemyFollowing.cs

--- Assets/EnemyFollowing.cs
+++ Assets/EnemyFollowing.cs
@@ -4,7 +4,7 @@ public class EnemyFollowing : MonoBehaviour
 {
     [Range(0.0f,1.0f)] public float speedRate;
     public Movement followingPlayer;
-    Vector2 direction;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,7 @@ public class EnemyFollowing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = Vector3.Normalize(followingPlayer.transform.position - transform.position);
-        transform.Translate(direction * speedRate * followingPlayer.m_moveSpeed * Time.deltaTime);
+        transform.LookAt(followingPlayer.transform.position);
+        transform.Translate(Vector3.forward * speedRate * followingPlayer.m_moveSpeed * Time.deltaTime);
     }
 }
