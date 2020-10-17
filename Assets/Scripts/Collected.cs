845124a50673032dedb9a44bbddc215c68a48156
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-10-17 18:31:28 +1100

    set bound for ground instead. Walls are now unnecessary

--- Assets/Scripts/Collected.cs
+++ Assets/Scripts/Collected.cs
@@ -5,29 +5,21 @@ public class Collected : MonoBehaviour
     public Player player;
     public int awardPoints = 100;
     public int healthChange = 100;
-    public GameObject wallLeft;
-    public GameObject wallRight;
-    public GameObject wallUp;
-    public GameObject wallDown;
+    public GameObject ground;
+    MeshRenderer rend;
 
 
     // Start is called before the first frame update
+    void Start () {
+        rend = ground.GetComponent<MeshRenderer>();
+        Vector3 newPos = setPos();
+        transform.position = newPos;
+    }
     void OnTriggerEnter(Collider collider) {
-        
-        float left = wallLeft.transform.position.x;
-        float right = wallRight.transform.position.x;
-        float up = wallUp.transform.position.z;
-        float down = wallDown.transform.position.z;
-        
 
         if (collider.gameObject.CompareTag("Player")){
-            //Vector3 newPos = generatePos(-3,-3,3,3);
-            Vector3 newPos = generatePos(left,down,right,up);
-            while (!isValid(newPos)) {
-                //newPos = generatePos(-3,-3,3,3);
-                newPos = generatePos(left,down,right,up);
-            }
-            Debug.Log(newPos);
+            
+            Vector3 newPos = setPos();
             Instantiate(this, newPos, this.transform.rotation);
             Destroy(gameObject);
             player.ChangeHealth(healthChange);
@@ -39,6 +31,21 @@ public class Collected : MonoBehaviour
         float xVal = Random.Range(minX,maxX);
         float zVal = Random.Range(minZ,maxZ);  
         return new Vector3(xVal,this.transform.position.y,zVal);
+    }
+
+    public Vector3 setPos() {
+
+        float left = rend.bounds.min.x;
+        float right = rend.bounds.max.x;
+        float up = rend.bounds.min.z;
+        float down = rend.bounds.max.z;
+
+        Vector3 newPos = generatePos(left,down,right,up);
+        while (!isValid(newPos)) {
+            newPos = generatePos(left,down,right,up);
+        }
+        //Debug.Log(newPos);
+        return newPos;
     }
     
     public bool isValid(Vector3 targetPos)
