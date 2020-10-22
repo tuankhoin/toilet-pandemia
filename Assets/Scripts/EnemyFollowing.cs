a4c5c8d17349ab84a11f21a18a0e48717d95841f
Hoang Anh Huy Luu <61114077+luuhoanganhhuy@users.noreply.github.com>
2020-10-22 20:06:32 +0700

    trigger enemies when nearby

--- Assets/Scripts/EnemyFollowing.cs
+++ Assets/Scripts/EnemyFollowing.cs
@@ -4,6 +4,7 @@ public class EnemyFollowing : EnemyBehavior
 {
     [Range(0.0f,1.0f)] public float speedRate;
     AudioSource audioSource;
+    private bool isFollowing = false;
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
@@ -14,6 +15,10 @@ public class EnemyFollowing : EnemyBehavior
     {
         float d = Vector3.Distance(followingPlayer.transform.position, transform.position);
         if (d < distance)
+        {
+            isFollowing = true;
+        }
+        if (isFollowing)
         {
             transform.LookAt(followingPlayer.transform.position);
             transform.Translate(Vector3.forward * speedRate * followingPlayer.speed * Time.deltaTime);
